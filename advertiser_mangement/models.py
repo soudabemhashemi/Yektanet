49e115142478db7b8363f4c790f195b4d0942316
soudabemhashemi <s.mhashemi.221@gmail.com>
2021-07-12 20:21:17 +0430

    add models to advertiser_management.models.py

--- advertiser_mangement/models.py
+++ advertiser_mangement/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+
+class Advertiser(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    # clicks = IntegerField()
+    # views = IntegerField()
+
+
+class Ad(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    imgUrl = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    clicks = models.IntegerField()
+    views = models.IntegerField()
